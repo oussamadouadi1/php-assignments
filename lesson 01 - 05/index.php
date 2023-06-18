14d5385a04962e976921cba49528de2dd9151d8e
OussamaDouadi <104687081+oussamadouadi1@users.noreply.github.com>
2023-06-18 23:36:07 +0100

    php-assignments

---
+++ lesson 01 - 05/index.php
@@ -0,0 +1,46 @@
+<!-- Assignment 01 -->
+<?php
+        /* This is a multi-line PHP comment
+           You can write anything you want here */
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php echo 'UTF-8'; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "My First PHP Page"; ?></title>
+</head>
+<body>
+<div><?php echo "We Love"; ?></div>
+<div><?php echo "Elzero Channel";?></div>
+</body>
+</html>
+
+<!-- Assignment 02 -->
+<?php 
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+
+?>
+
+<!-- Assignment 03 -->
+<?php
+
+// My Application
+// Version 1.0
+// Created By Elzero
+?>
+
+<!-- Assignment 04 -->
+
+
+<?php
+
+// First Comment
+# Second Comment
+/* Third Comment */
+// Fourth Comment
+
+?>
